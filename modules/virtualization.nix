a757752ee1b8c8a164d6c669edce033da4521073
imaro56 <dimamarich07@gmail.com>
2026-03-09 14:43:23 +0200

    Fix virtual machine

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -4,6 +4,10 @@
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 
+  # Workaround: virt-secret-init-encryption.service hardcodes /usr/bin/sh
+  # which doesn't exist on NixOS, causing activation failures
+  systemd.services.virt-secret-init-encryption.enable = false;
+
   virtualisation.docker.enable = true;
   virtualisation.docker.enableOnBoot = false;
 }
